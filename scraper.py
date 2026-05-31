706d768fe09d494dd9a68968662688869cf55d9d
cygane <juliacygan46@gmail.com>
2026-05-31 12:51:28 +0200

    cleaning the data and saving it in the json

--- scraper.py
+++ scraper.py
@@ -28,6 +28,36 @@ else:
     
     if(script_tag):
         logging.info(f"Json found")
-        print(script_tag.string[:500])
+        data_json = json.loads(script_tag.string)
+        ads = data_json['props']['pageProps']['data']['searchAds']['items']
+
+        if(ads):
+            logging.info(f"Advertisements scraped")
+            cleaned_data = []
+            for ad in ads:
+                data = {}
+                data['id'] = ad.get('id')
+                data['slug'] = ad.get('slug')
+
+                location = ad.get('location')
+                if(location and location.get('reverseGeocoding') and len(location.get('reverseGeocoding').get('locations', [])) > 2):
+                    data['district'] = location['reverseGeocoding']['locations'][2].get('name')
+                else:
+                    data['district'] = None
+
+                data['roomsNumber'] = ad.get('roomsNumber')
+                data['floorNumber'] = ad.get('floorNumber')
+                data['pricePerSquareMeter'] = ad.get('pricePerSquareMeter').get('value') if ad.get('pricePerSquareMeter') else None
+                data['totalPrice'] = ad.get('totalPrice').get('value') if ad.get('totalPrice') else None
+                data['areaInSquareMeters'] = ad.get('areaInSquareMeters')
+                data['rentPrice'] = ad.get('rentPrice').get('value') if ad.get('rentPrice') else None
+                cleaned_data.append(data)
+
+            with open("results.json", "w", encoding="utf-8") as f:
+                json.dump(cleaned_data, f, indent=4, ensure_ascii=False)
+            
+            logging.info(f"Saved {len(cleaned_data)} advertisements in file results.json")
+        else:
+            logging.error(f"List of advertisements not found")
     else:
         logging.error(f"Json not found")
